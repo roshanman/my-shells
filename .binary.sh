496e715e9a76bbc29f911cca72999c8e69eb6666
zhang xiuming <zhangxiuming@zepp.com>
2022-08-22 14:07:12 +0800

    2022.8.22

--- .binary.sh
+++ .binary.sh
@@ -3,7 +3,7 @@
 name=$1
 version=$2
 
-url=$(echo "https://cart.athuami.com/release/$version/binary/$name.podspec.json" | sed 's/,//g')
+url=$(echo "https://internal-artifactory.zepp.top/release/$version/binary/$name.podspec.json" | sed 's/,//g')
 
 curl $url | jq
 curl $url | jq ".source.http"
